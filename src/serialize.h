cad323f630636bde5cd86bbb0e3c6b3dc674e8b0
Anthony Towns <aj@erisian.com.au>
2026-02-26 20:53:36 +1000

    serialize: string_view serialization
    Allows `stream << sv` to serialize from a string_view directly, without
    converting to a string or similar first.

--- src/serialize.h
+++ src/serialize.h
@@ -24,6 +24,7 @@
 #include <set>
 #include <span>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -693,6 +694,12 @@ template<typename Stream, typename C> void Serialize(Stream& os, const std::basi
 template<typename Stream, typename C> void Unserialize(Stream& is, std::basic_string<C>& str);
 
 /**
+ *  string_view
+ */
+template<typename Stream, typename C> void Serialize(Stream& os, const std::basic_string_view<C>& str);
+template<typename Stream, typename C> void Unserialize(Stream& is, std::basic_string_view<C>& str) = delete;
+
+/**
  * prevector
  */
 template<typename Stream, unsigned int N, typename T> inline void Serialize(Stream& os, const prevector<N, T>& v);
@@ -794,7 +801,16 @@ void Unserialize(Stream& is, std::basic_string<C>& str)
         is.read(MakeWritableByteSpan(str));
 }
 
-
+/**
+ * string_view
+ */
+template<typename Stream, typename C>
+void Serialize(Stream& os, const std::basic_string_view<C>& str)
+{
+    WriteCompactSize(os, str.size());
+    if (!str.empty())
+        os.write(MakeByteSpan(str));
+}
 
 /**
  * prevector
