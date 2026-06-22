1e335f177e2cacce12314b84d75f4672f2efa735
Fabian Jahr <fjahr@protonmail.com>
2026-06-22 14:29:39 +0300

    index: Fix unflushed chain commit skipping logs

--- src/index/base.cpp
+++ src/index/base.cpp
@@ -281,10 +281,14 @@ bool BaseIndex::Commit()
     if (ok) {
         // Don't commit if the index is ahead of the chainstate's last flushed block
         const CBlockIndex* index_tip = m_best_block_index.load();
-        if (!m_chain->isAncestorOfLastFlushedBlock(index_tip->GetBlockHash())) {
-            LogInfo("Skipping commit, index is ahead of flushed chainstate (index height %d, chain height %d)",
-                    index_tip->nHeight, m_chain->getHeight().value_or(-1));
-            return false;
+        {
+            LOCK(::cs_main);
+            const CBlockIndex* last_flushed = m_chainstate->GetLastFlushedBlock();
+            if (!last_flushed || last_flushed->GetAncestor(index_tip->nHeight) != index_tip) {
+                LogInfo("Skipping commit, index (height %d) is ahead of the last flushed block (height %d)",
+                        index_tip->nHeight, last_flushed ? last_flushed->nHeight : -1);
+                return false;
+            }
         }
         CDBBatch batch(GetDB());
         ok = CustomCommit(batch);
